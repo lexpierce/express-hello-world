75b77921e8b79f98c056bda7214891290acb588d
Alexander Sisco <alexander@render.com>
2024-11-07 10:59:26 -0800

    Update to ExpressJS 5, canvas-confetti 1.9.3, and bun

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const html = `
 <html>
   <head>
     <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.3/dist/confetti.browser.min.js"></script>
     <script>
       setTimeout(() => {
         confetti({
